20740b8f155ba03566e716e421efccee5268fb83
Sam Day <me@samcday.com>
2022-10-11 16:54:26 +0200

    spooky voodoo to backup k3s etcd maybe

--- kube-system/terraform/main.tf
+++ kube-system/terraform/main.tf
@@ -65,6 +65,29 @@ resource "b2_application_key" "fly-io-headscale-backups" {
   capabilities = ["listFiles", "readFiles", "writeFiles", "deleteFiles"]
 }
 
+resource "b2_bucket" "hominion-cluster-backups" {
+  bucket_name = "samcday-hominion-cluster-backups"
+  bucket_type = "allPrivate"
+}
+
+resource "b2_application_key" "hominion-cluster-backups" {
+  key_name     = "fly-io"
+  bucket_id    = b2_bucket.hominion-cluster-backups.bucket_id
+  capabilities = ["listFiles", "readFiles", "writeFiles", "deleteFiles"]
+}
+
+resource "kubernetes_secret" "kube-system-cluster-backups-bucket" {
+  metadata {
+    name      = "cluster-backups-bucket"
+    namespace = "kube-system"
+  }
+
+  data = {
+    etcd-s3-access-key = b2_application_key.hominion-cluster-backups.application_key_id
+    etcd-s3-secret-key = b2_application_key.hominion-cluster-backups.application_key
+  }
+}
+
 resource "b2_bucket" "synapse-backups" {
   bucket_name = "samcday-synapse-backups"
   bucket_type = "allPrivate"
@@ -76,7 +99,7 @@ resource "b2_application_key" "synapse-backups" {
   capabilities = ["listFiles", "readFiles", "writeFiles", "deleteFiles"]
 }
 
-resource "kubernetes_secret" "synapse-backups-creds" {
+resource "kubernetes_secret" "synapse-backup-bucket" {
   metadata {
     name      = "backup-bucket"
     namespace = "synapse"
