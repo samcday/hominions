fcf670ad9bd1955022f18b5f315b8a52f036520b
Sam Day <me@samcday.com>
2023-01-18 12:48:56 +0100

    fp2-1.hominions.tailnet.samcday.com

--- devices/fp2-1.hominions.tailnet.samcday.com/build.sh
+++ devices/fp2-1.hominions.tailnet.samcday.com/build.sh
@@ -14,8 +14,33 @@ if [[ ! -d $dir/cache_git/pmaports ]]; then
     popd
 fi
 
-pmbootstrap -y -c $cfg install --password test123
+pmbootstrap -y -c $cfg install --password test123 --no-firewall
 
-echo fp2-1.hominions.tailnet.samcday.com | sudo tee $dir/chroot_rootfs_fairphone-fp2/etc/hostname
+chroot=$dir/chroot_rootfs_fairphone-fp2
+
+sudo cat > $chroot/etc/modules-load.d/k8s.conf <<HERE
+overlay
+br_netfilter
+HERE
+
+sudo cat > $chroot/etc/sysctl.d/k8s.conf <<HERE
+sysctl net.bridge.bridge-nf-call-iptables=1
+sysctl net.bridge.bridge-nf-call-ip6tables=1
+sysctl net.ipv4.ip_forward=1
+HERE
+
+sudo cat > $chroot/etc/local.d/restartnm.start <<HERE
+service networkmanager restart
+service ntpd restart
+HERE
+chmod +x $chroot/etc/local.d/restartnm.start
+
+sed -i 's/bin_dir = .*/bin_dir = "/opt/cni/bin"/g' $chroot/etc/containerd/config.toml
+
+pmbootstrap chroot -r rc-update add containerd
+pmbootstrap chroot -r rc-update add kubelet
+pmbootstrap chroot -r rc-update add tailscale
+
+pmbootstrap -y -c $cfg install --password test123 --no-firewall
 
 pmbootstrap -y -c $cfg export
