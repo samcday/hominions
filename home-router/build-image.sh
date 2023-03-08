ed18c6df568c4fe5e6ad4f97a95e45734b09d6f6
Sam Day <me@samcday.com>
2023-03-08 17:10:24 +0100

    home-router: rejigger for betterness

--- home-router/build-image.sh
+++ home-router/build-image.sh
@@ -2,13 +2,23 @@
 cd "$(dirname "$0")"
 set -uexo pipefail
 
-if [[ ! -d .imagebuilder/ ]]; then
-  mkdir .imagebuilder
+if [[ ! -f _build/.setup ]]; then
+  mkdir -p _build/
   curl -s --retry 2 --fail -L https://downloads.openwrt.org/releases/22.03.3/targets/ipq40xx/generic/openwrt-imagebuilder-22.03.3-ipq40xx-generic.Linux-x86_64.tar.xz | \
-    tar --strip-components=1 -C .imagebuilder/ -Jxvf -
+    tar --strip-components=1 -C _build/ -Jxvf -
+  touch _build/.setup
 fi
 
+cp -R files/ _build/
+
+export BIN_DIR="."
+export FILES="files"
+export PACKAGES=$(echo $(cat packages))
+export PROFILE=avm_fritzbox-4040
+
 (
-  cd .imagebuilder/
-  make image PROFILE="avm_fritzbox-4040" PACKAGES="luci tailscale openssh-sftp-server htop" FILES="../files"
+  env
+  exit
+  cd _build/
+  make image
 )
