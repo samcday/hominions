dd9730dd530ce2985f61170a560b75e670ae5552
Sam Day <me@samcday.com>
2023-03-21 22:38:21 +0100

    rock64-1: fix dns and join tailnet

--- rock64-1/flash.sh
+++ rock64-1/flash.sh
@@ -2,7 +2,17 @@
 cd "$(dirname "$0")"
 set -uexo pipefail
 
+# Secrets can be set explicitly, otherwise they're taken from my Bitwarden vault by default.
+if [[ -z "${BW_SESSION:-}" ]]; then
+  export BW_SESSION=$(bw unlock --raw)
+fi
+
+# Secrets
+export TAILNET_AUTH_KEY=${TAILNET_AUTH_KEY:-$(bw get item "6e22f9a5-38aa-4703-8dfd-afc200fcb3ee" | jq -r .notes)}
+
 dev=$1
+
+for d in $dev*; do umount $d || true; done
 
 sfdisk $dev <<HERE
 label: dos
@@ -19,6 +29,8 @@ trap "umount $mntdir" EXIT
 mount ${dev}1 $mntdir
 
 rsync -avP image/ $mntdir/
+
+echo $TAILNET_AUTH_KEY > $mntdir/etc/ts-auth-key
 
 # Bootloader setup
 dd if=$mntdir/boot/rksd_loader.img of=$dev seek=64 conv=notrunc
