92953a46897b173360161a25e583825b19d2120f
Sam Day <me@samcday.com>
2023-04-02 23:45:49 +0200

    here goes nuffin

--- home-cluster/bootstrap.sh
+++ home-cluster/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 kubectl apply -f kube-flannel/kube-flannel.yaml
+helm repo add fluxcd-community https://fluxcd-community.github.io/helm-charts
+helm repo update fluxcd-community
 helm install flux fluxcd-community/flux2 --values flux-values.yaml -n flux-system --create-namespace
 sops -d secrets/flux-system/age-key.yaml | kubectl apply -f-
+kubectl apply -k flux-system
