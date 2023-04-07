47cac51da8e942fb2a5388fb80a7b0313eabd608
Sam Day <me@samcday.com>
2023-04-07 09:42:52 +0200

    home-cluster/monitoring: fix TF

--- home-cluster/monitoring/main.tf
+++ home-cluster/monitoring/main.tf
@@ -1,13 +1,9 @@
 terraform {
   backend "kubernetes" {
-    secret_suffix = "state"
+    secret_suffix = "terraform"
     namespace     = "monitoring"
   }
   required_providers {
-    b2 = {
-      source  = "Backblaze/b2"
-      version = "0.8.4"
-    }
     dmsnitch = {
       source  = "plukevdh/dmsnitch"
       version = "0.1.4"
@@ -19,32 +15,8 @@ terraform {
   }
 }
 
-provider "b2" {}
 provider "dmsnitch" {}
 provider "kubernetes" {}
-
-resource "b2_bucket" "grafana-backups" {
-  bucket_name = "samcday-grafana-backups"
-  bucket_type = "allPrivate"
-}
-
-resource "b2_application_key" "grafana-backups" {
-  key_name     = "grafana"
-  bucket_id    = b2_bucket.grafana-backups.bucket_id
-  capabilities = ["listFiles", "readFiles", "writeFiles", "deleteFiles"]
-}
-
-resource "kubernetes_secret" "grafana-backups-bucket" {
-  metadata {
-    name      = "grafana-backups-bucket"
-    namespace = "monitoring"
-  }
-
-  data = {
-    LITESTREAM_ACCESS_KEY_ID     = b2_application_key.grafana-backups.application_key_id
-    LITESTREAM_SECRET_ACCESS_KEY = b2_application_key.grafana-backups.application_key
-  }
-}
 
 resource "dmsnitch_snitch" "home-cluster" {
   name = "home-cluster"
