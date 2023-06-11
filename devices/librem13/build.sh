3b971e144075bbd3ee8f7fbf9c345f4c09ea4715
Sam Day <me@samcday.com>
2023-06-11 09:57:05 +0200

    devices/librem13: bake secrets into image

--- devices/librem13/build.sh
+++ devices/librem13/build.sh
@@ -7,21 +7,28 @@ if [[ -z "${BW_SESSION:-}" ]]; then
   export BW_SESSION=$(bw unlock --raw)
 fi
 
-export TAILNET_AUTH_KEY=${TAILNET_AUTH_KEY:-$(bw get item "6e22f9a5-38aa-4703-8dfd-afc200fcb3ee" | jq -r .notes)}
+rm -rf mkosi.extra/
+cp -R mkosi.{real-extra,extra}
+
+echo ${TAILNET_AUTH_KEY:-$(bw get item "6e22f9a5-38aa-4703-8dfd-afc200fcb3ee" | jq -r .notes)} > $mntdir/etc/ts-auth-key
+
+sudo mkosi -f
+
+if [[ -n "$BUILD_ONLY" ]]; then
+  exit
+fi
 
 dev=$1
 
 for d in $dev*; do umount $d || true; done
 
-# dd if=image.raw of=$dev bs=4M status=progress
-# sync
+dd if=image.raw of=$dev bs=4M status=progress
+sync
 
 mntdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'librem13-prep')
 trap "umount $mntdir/boot/efi; umount $mntdir" EXIT
 mount ${dev}3 $mntdir
 mount ${dev}1 $mntdir/boot/efi
-
-echo $TAILNET_AUTH_KEY > $mntdir/etc/ts-auth-key
 
 # Setup GRUB
 cp image.initrd $mntdir/boot/initramfs-linux.img
