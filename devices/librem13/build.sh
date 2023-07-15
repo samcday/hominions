a76c009e66be503220419b919b94129eec753ae3
Sam Day <me@samcday.com>
2023-07-15 12:06:55 +0200

    Never mind, CoreOS (and Flatcar) don't boot at all on this laptop....
    Revert "librem13: give up on immutable Arch hax, use CoreOS"
    
    This reverts commit b278e3cbb852f607e6e6c802e9184ed8410dd626.

---
+++ devices/librem13/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+set -ueo pipefail
+
+dev=$1
+
+if [[ -z "${SKIP_BUILD:-}" ]]; then
+  sops -d mkosi.rootpw.enc > mkosi.rootpw
+  chmod 600 mkosi.rootpw
+
+  rm -rf mkosi.extra/
+  cp -R mkosi.{real-extra,extra}
+
+  for f in $(find mkosi.extra/ -type f -name '*.enc*'); do
+    echo decrypting $f
+    sops -i -d $f
+    mv $f $(echo $f | sed s/\.enc//)
+  done
+
+  chmod 400 mkosi.extra/etc/cryptsetup-keys.d/root.key
+  sudo mkosi -f --bios-size=1M
+fi
+
+if [[ -n "${BUILD_ONLY:-}" ]]; then
+  exit
+fi
+
+for d in $dev*; do sudo umount $d || true; done
+
+sudo dd if=image.raw of=$dev bs=4M status=progress conv=fsync oflag=sync
+
+mntdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'librem13-prep')
+trap "sudo umount $mntdir/boot/efi; sudo umount $mntdir" EXIT
+sudo mount ${dev}3 $mntdir
+sudo mount ${dev}1 $mntdir/boot/efi
+
+# Setup GRUB
+sudo cp image.initrd $mntdir/boot/initramfs-linux.img
+sudo cp image.vmlinuz $mntdir/boot/vmlinuz-linux
+
+sudo arch-chroot $mntdir grub-install --target=i386-pc $dev
+sudo arch-chroot $mntdir grub-mkconfig -o /boot/grub/grub.cfg
+
+# Tweak kernel params - read only filesystem + disable audit
+sudo sed -i -e "s/quiet/audit=0/" -e "s/ rw / /" $mntdir/boot/grub/grub.cfg
--- home-cluster/README.md
+++ home-cluster/README.md
@@ -5,7 +5,7 @@ Early bootstrapping:
 k3sup install --host librem13.hominions.tailnet.samcday.com --k3s-channel v1.26 --cluster --k3s-extra-args "--container-runtime-endpoint=/run/containerd/containerd.sock --node-ip=$(tailscale ip -4 librem13.hominions.tailnet.samcday.com) --flannel-backend=none --disable=traefik --disable=servicelb --disable=local-storage"
 kubectl apply -f home-cluster/kube-system/ip-masq-agent.yaml
 
-helm install flux flux2 --repo=https://fluxcd-community.github.io/helm-charts -n flux-system --create-namespace
+helm install flux flux2 -repo=https://fluxcd-community.github.io/helm-charts -n flux-system --create-namespace
 helm install -n kube-system tailscale-node-controller tailscale-node-controller --repo https://samcday.github.io/tailscale-node-controller
 
 sops -d home-cluster/secrets/flux-system/age-key.yaml | kubectl apply -f-
